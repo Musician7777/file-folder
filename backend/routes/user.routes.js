d0058fbf9910d105268dbb618b173a3289c2267c
“Musician” <hhajriwnko@gmail.com>
2024-12-28 00:08:33 +0530

    User login done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -10,5 +10,9 @@ router.post('/register',[
 ],
     userController.registerUser
 )
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid email'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 chars long')
+], userController.loginUser)
 
 module.exports = router;
